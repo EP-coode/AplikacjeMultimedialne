ce466cfe5d26fe4c1275159bea2c026e155ec5b1
Ernest Przybyl <ernestprzybyl@gmail.com>
2022-04-06 12:24:57 +0200

    add required liblaries

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Add as AddIcon } from "@mui/icons-material";
+import { Fab } from "@mui/material";
 
 function App() {
   return (
@@ -19,6 +27,24 @@ function App() {
           Learn React
         </a>
       </header>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard - Hello mui
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      <Fab color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
     </div>
   );
 }
