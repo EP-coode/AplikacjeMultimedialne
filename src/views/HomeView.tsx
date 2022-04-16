96fed87341c1ae40b8ff524c13456e698f5c08db
EP-coode <ernestprzybyl@gmail.com>
2022-04-16 19:55:39 +0200

    add lint script, remove warnings

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -7,7 +7,6 @@ import { IArticle } from "../api/interfaces/IArticle";
 import { AritclesService } from "../api/ArticlesService";
 import ArticleGrid from "../components/ArticleGrid";
 import SearchInput from "../components/SearchInput";
-import FavouriteArticlesService from "../storeServices/FavouriteArticlesService";
 
 const ARTICLES_PER_FETCH = 10;
 
